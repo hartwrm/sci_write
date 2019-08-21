557c7795ab751f7df5b68e0f9ed7b25916e290c6
Andrew Hartzell <hartwrm@Macs-MacBook-Pro.local>
2019-08-20 23:04:09 -0400

    messing with s3 configs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Prompt.create([{writing_prompt: "It’s not really a fly. If you swat it, they’ll just fine you and send two more."}, {writing_prompt: "We’re headed to Madam Maxime’s at the edge of the universe."}, {writing_prompt: "Why grieve when you can keep your loved one encased in Forever Glass™?"}, {writing_prompt: "A group of intelligence-enhanced lab animals must find a way to communicate before their turn in the euthanization chamber."}])
-
-CrowdStory.create([{content: "Your Story Here"}])
-
-p "seeded db"
+# Prompt.create([{writing_prompt: "It’s not really a fly. If you swat it, they’ll just fine you and send two more."}, {writing_prompt: "We’re headed to Madam Maxime’s at the edge of the universe."}, {writing_prompt: "Why grieve when you can keep your loved one encased in Forever Glass™?"}, {writing_prompt: "A group of intelligence-enhanced lab animals must find a way to communicate before their turn in the euthanization chamber."}])
+#
+# CrowdStory.create([{content: "Your Story Here"}])
+#
+# p "seeded db"
