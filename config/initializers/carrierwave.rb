f366362904f10416f00ab982887323bd2ab3e1d0
hartwrm <47437949+hartwrm@users.noreply.github.com>
2019-08-24 09:02:12 -0400

    config aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AWS_ACCESS_KEY_ID',                        # required unless using use_iam_profile
-    aws_secret_access_key: 'AWS_SECRET_ACCESS_KEY',                        # required unless using use_iam_profile
+    aws_access_key_id:     'AWS_ACCESS_KEY',                        # required unless using use_iam_profile
+    aws_secret_access_key: 'AWS_SECRET_KEY',                        # required unless using use_iam_profile
     region:                'us-east-1',                  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'S3_BUCKET'                                      # required
